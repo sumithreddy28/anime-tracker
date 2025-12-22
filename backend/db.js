475aa00e2c7e840b998db04eb36ee07422f6976c
sumtihreddy <110587056+sumtihreddy@users.noreply.github.com>
2025-12-23 00:19:13 +0530

    updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,15 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL ? { rejectUnauthorized: true } : false
-})
+  ssl: process.env.DB_SSL === "true"
+    ? { rejectUnauthorized: true }
+    : false,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 export default pool;
