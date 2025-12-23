e449b5863c525f9402bff32e5e07cb9282797e42
sumtihreddy <110587056+sumtihreddy@users.noreply.github.com>
2025-12-23 10:44:04 +0530

    test

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === "true"
-    ? { rejectUnauthorized: true }
-    : false,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default pool;
