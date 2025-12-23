12c3854a1c605eb3146e775e527493ae6cd2eb71
sumtihreddy <110587056+sumtihreddy@users.noreply.github.com>
2025-12-23 11:20:24 +0530

    ssl fix

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
