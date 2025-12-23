7ea5018da335d82b77f5b16e74fb9e1ba714b35c
sumtihreddy <110587056+sumtihreddy@users.noreply.github.com>
2025-12-23 12:22:26 +0530

    db change

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,5 @@
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-
-  ssl: {
-    rejectUnauthorized: false
-  },
-
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from(process.env.DB_PASSWORD)
-  },
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
